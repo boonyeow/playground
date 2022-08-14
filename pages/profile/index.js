b5446a73c00821953048c3b6a0371db33fc5a471
its-seb <51190277+its-seb@users.noreply.github.com>
2022-08-14 23:46:23 +0800

    Update profile index

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -16,18 +16,64 @@ import Sidebar from "../../components/Sidebar";
 import PageHeader from "../../components/PageHeader";
 import { useEffect, useState } from "react";
 import SingleListing from "../../components/SingleListing";
-import NextImage from "next/image";
+import axios from "axios";
+import cfg from "../../util/config";
+import IconService from "icon-sdk-js";
+import ICONexConnection from "../../util/interact";
+
+const {
+    IconConverter,
+    IconBuilder,
+    HttpProvider,
+    SignedTransaction,
+    IconWallet,
+} = IconService;
 
 const Profile = () => {
     const [userInfo, setUserInfo] = useState({
         userAddress: 0,
         projectsDeployed: [],
     });
+    const connection = new ICONexConnection();
+    const [projectList, setProjectList] = useState([]);
+    const [involvedProjects, setInvolvedList] = useState([]);
 
     useEffect(() => {
+        const fetchProjects = async () => {
+            let res = await axios.get(`${cfg.BASE_URL}/api/projects/fetch`);
+            setProjectList(res.data);
+        };
+
         const temp = localStorage.getItem("_persist");
         temp = temp == null ? userInfo : JSON.parse(temp);
         setUserInfo(temp);
+        fetchProjects();
+
+        const getBalance = async (contractAddress) => {
+            const call = new IconBuilder.CallBuilder()
+                .method("balanceOf")
+                .to(contractAddress)
+                .params({ _owner: userInfo.userAddress })
+                .build();
+            return await connection.iconService.call(call).execute();
+        };
+
+        if (projectList.length != 0) {
+            let counter = 0;
+            const involvedProjectsData = [];
+
+            projectList.map((currentProject, index) => {
+                if (index != 0) {
+                    getBalance(currentProject.contractAddress).then((res) => {
+                        if (IconConverter.toNumber(res) > 0) {
+                            involvedProjectsData[counter] = JSON.parse(JSON.stringify(currentProject));
+                            counter++;
+                        }
+                    });
+                }
+            });
+            setInvolvedList(involvedProjectsData);
+        };
     }, []);
 
     return (
@@ -55,12 +101,26 @@ const Profile = () => {
                             mt="15px"
                             width="100%"
                             spacingY="20px"
-                        ></SimpleGrid>
+                        >
+                            {involvedProjects.length > 0 &&
+                                involvedProjects.map((currentProject, index) => {
+                                    return (
+                                        <>
+                                            <SingleListing
+                                                title={currentProject.name}
+                                                src={currentProject.thumbnailSrc}
+                                                seed={currentProject.contractAddress}
+                                            />
+                                        </>
+                                    )
+                                })
+                            }
+                        </SimpleGrid>
                     </Box>
                 </Box>
             </Box>
         </>
-    );
-};
+    )
+}
 
-export default Profile;
+export default Profile
